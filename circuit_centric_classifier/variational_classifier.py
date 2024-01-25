81dc369d9d76fab6282fa46b06c2e0abc8683c2c
ancastillov <ancastillov@unal.edu.co>
2024-01-24 19:03:09 -0500

    Variational clasifier, falta normalizar el entorno antes y la medición del final

--- circuit_centric_classifier/variational_classifier.py
+++ circuit_centric_classifier/variational_classifier.py
@@ -1,38 +1,58 @@
 import tensorcircuit as tc
 import numpy as np
 from tensorcircuit.templates.dataset import amplitude_encoding
+from tensorcircuit import keras
+from functools import partial
+import tensorflow as tf
 
-tc.backend("tensorflow")
+tc.set_backend("tensorflow")
 tc.set_dtype("complex128")
 
 class VariationalClassifier():
 
-    def __init__(self, nqubits:int):
+    def __init__(self, nqubits:int, rangeList:list):
         self.circuit = tc.Circuit(nqubits)
+
+        self.n = self.circuit._nqubits
+        self.rangeList = rangeList
+        self.stepList = [self.n//np.gcd(self.n, c) for c in self.rangeList]
+
+        self.model = tf.keras.Sequential(
+            [self.quantumLayer(r, s) for r, s in zip(self.rangeList, self.stepList)]
+            )
         
-
-    def gate_G(self, index, alpha, beta, gamma, phi):
-
-        # Define G gate
-        unitary_ = np.e**(1.j*phi) * np.array(
-            [[np.e**(1.j * beta)*np.cos(alpha),
-              np.e**(1.j * gamma)*np.sin(alpha)],
-              [-np.e**(-1.j * gamma)*np.sin(alpha),
-              -np.e**(-1.j * beta)*np.cos(alpha)]])
+        self.model.append(keras.QuantumLayer(partial(self.measure)))
         
-        # Create the unitary matrix
-        #G = tc.gates.Unitary(unitary_, wires=1)
-        G = self.circuit.unitary(index, unitary=unitary_, name="G")
+        self.model.compile(
+            loss=tf.keras.losses.Hinge(), 
+            optimizer=tf.keras.optimizers.legacy.Adam(0.01),
+            metrics=["binary_accuracy"]
+        )
 
-        
+    def layer(self, c_range:int, weights:np.array): #weight debe ser nqubits+steps x 3
 
-    # def layer(self, c_range:int):
+        steps = self.n//np.gcd(self.n, c_range)
+        q_i = 0
 
-    #     steps = np.gcd(self.circuit._nqubits, c_range)
-    #     q_i = 0
-    #     for i in range(steps):
-    #         next_q_i = (q_i - c_range) % self.circuit._nqubits
-    #         gate_G(next_q_i, alpha, beta, gamma, phi)
+        for i in range(self.n):
+            self.circuit.u(i, theta=weights[i,0], phi=weights[i,1], lbd=weights[i,2])
+
+        for j in range(steps):
+            next_q_i = (q_i - c_range) % self.n 
+            self.circuit.cu(q_i, next_q_i, theta=weights[self.n+j,0], phi=weights[self.n+j,1], lbd=weights[self.n+j,2])   
+            q_i = next_q_i
+
+    def quantumLayer(self, c_range, steps):
+        return keras.QuantumLayer(partial(self.layer, c_range=c_range), [(self.n + steps, 3)])
+    
+    def fit(self, x_train, y_train, batch_size, epoch):
+        self.model.fit(x_train, y_train, batch_size, epoch)   
+
+    def measure(self):
+        return tc.backend.real(self.circuit.measure(0))
+         
+
+            
         
 
     
