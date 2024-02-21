f636f500ff281c78a100424b1684c0ac2a945d82
Sebastian Molina <smolinadunal.edu.co>
2024-02-21 16:12:08 -0500

    Circuit-centric VC with comments

--- qmc/circuit_centric_classifier/variational_classifier.py
+++ qmc/circuit_centric_classifier/variational_classifier.py
@@ -1,15 +1,14 @@
 import tensorcircuit as tc
 import numpy as np
-from tensorcircuit.templates.dataset import amplitude_encoding
-from tensorcircuit import keras
-from functools import partial
 import tensorflow as tf
 
 K = tc.set_backend("tensorflow")
 tc.set_dtype("complex128")
 
-class VariationalClassifier():
-
+class VariationalClassifier():  
+    """
+    Defines a variational circuit in the way of 'Circuit-centric qunatum classifiers' by Schuld, et al (2018).
+    """
     def __init__(self, nqubits:int=10, rangeList:list=[1,2,3]):
         self.circuit = None
 
@@ -24,14 +23,16 @@ class VariationalClassifier():
             )
         ])
 
-
+    """
+    Given a list of ranges —see Schuld, et al. (2018)—, creates the variational circuits and its corresponding layers.
+    """
     def layer(self, x, weights): 
 
         self.circuit = tc.Circuit(self.nqubits, inputs=x)
         
         # Iterates over the list of "jump" ranges
         for k in range(len(self.rangeList)):
-
+            # Applies the first iteration of U-gates.
             for i in range(self.nqubits):
                 self.circuit.u(
                     i, 
@@ -42,7 +43,7 @@ class VariationalClassifier():
 
             steps = self.nqubits//np.gcd(self.nqubits, self.rangeList[k])
             q_i = 0
-
+            # Applies the iteration of controlled U-gates with the corresponding range, i.e, the index of the qubit to be controlled. For instance, with range 3, starting by q_0, the controlled qubit is q_5. After that, q_5 controls q_2, and so on.
             for j in range(1, steps+1):
                 next_q_i = (q_i - self.rangeList[k]) % self.nqubits
                 self.circuit.cu(
@@ -53,14 +54,15 @@ class VariationalClassifier():
                     lbd=weights[k,self.nqubits+j,2]
                 )   
                 q_i = next_q_i
-
+        # Applies the 'bias' gate, previous to measuring the circuit.
         self.circuit.u(
             0, 
             theta=weights[len(self.rangeList),0,0], 
             phi=weights[len(self.rangeList),0,1], 
             lbd=weights[len(self.rangeList),0,2]
         )      
-                
+        
+        # Measurement
         outputs = K.stack(
             [K.real(self.circuit.expectation([tc.gates.z(), [i]])) for i in range(self.nqubits)] + 
             [K.real(self.circuit.expectation([tc.gates.x(), [i]])) for i in range(self.nqubits)])
